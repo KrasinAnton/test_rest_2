8a4a5db02733afb675c56868b00c9d0c4322621e
KrasinAnton <2saper@mail.ru>
2023-05-23 20:34:07 +0500

    lr15

---
+++ src/test/java/test_rest_service/TestRest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package test_rest_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
